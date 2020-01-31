cab90fc6354514fe2696a658b9b12a7a2648116c
Piotr <piotr.romatowski@gmail.com>
2020-01-31 19:09:07 +0100

    js-dodaj-usun-tekst

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+let przycisk1 = document.getElementById('dodaj');
+let przycisk2 = document.getElementById('usun');
+let paragraf = document.getElementById('tekst');
+
+przycisk1.onclick = function () {
+paragraf.textContent="Akademia108"
+};
+
+przycisk2.onclick = function () {
+paragraf.textContent=null
+};
+    
